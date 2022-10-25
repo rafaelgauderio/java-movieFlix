c18fb992c301bb47fa30ed00df40b58df7d503aa
Rafael De Luca <deluca1712@gmail.com>
2022-10-25 20:21:05 -0300

    Class UserService and CLass UserDetails

---
+++ src/main/java/com/rafaeldeluca/movieflix/services/UserService.java
@@ -0,0 +1,35 @@
+package com.rafaeldeluca.movieflix.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.rafaeldeluca.movieflix.entities.User;
+import com.rafaeldeluca.movieflix.repositories.UserRepository;
+
+public class UserService implements UserDetailsService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserRepository userRepository;	
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		User user = userRepository.findByEmail(email);
+		
+		if (user == null) {
+			logger.error("O usuário não foi encontrado: " + email);
+			throw new UsernameNotFoundException("O email informado não foi encontrado");
+		}
+		
+		logger.info("Usuário encontrado: " + email);
+		
+		return user;
+	}
+
+}
