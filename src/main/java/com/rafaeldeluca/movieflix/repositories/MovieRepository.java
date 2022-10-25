183cb86d8735cbc3d8f5598329331463263b7606
Rafael de Luca <deluca1712@gmail.com>
2022-10-25 12:25:15 -0300

    Interfaces Repositories exten that ds JpaRepository

---
+++ src/main/java/com/rafaeldeluca/movieflix/repositories/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.rafaeldeluca.movieflix.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaeldeluca.movieflix.entities.Movie;
+
+@Repository
+public interface MovieRepository extends JpaRepository <Movie, Long>{
+
+}
---
+++ src/main/java/com/rafaeldeluca/movieflix/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.rafaeldeluca.movieflix.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaeldeluca.movieflix.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+
+}
